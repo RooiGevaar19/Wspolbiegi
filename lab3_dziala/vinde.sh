6be62cc808c8ec82eb8d552255e141fc612a91a7
RooiGevaar19 <redthreat19@gmail.com>
2018-05-14 18:32:39 +0200

    Drugi commit XD

---
+++ lab3_dziala/vinde.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+if [[ $# -eq 0 ]]; then
+	x=`./vind .`
+elif [[ $# -eq 1 ]]; then
+	x=`./vind $1 *`
+else 
+	x=`./vind $1 $2`
+fi
+
+if [[ "a$x" == "a" ]]; then
+	echo "Nic nie znaleziono.";
+else
+	echo "$x";
+fi
